6e6d1da8ff889ba14df1cd1e577a5a715af95c5d
ingalin <ingalin@inbox.lv>
2020-12-16 10:12:17 -0500

    first files

---
+++ script.js
@@ -0,0 +1,50 @@
+// const app = {};
+// app.cliksPut = function () {
+
+//     let classTrue = false;
+
+//     $(".carousel-cell").off("click").on('click', function () {
+
+        
+//     if (classTrue == true) {
+//         // console.log(classTrue)
+//         // console.log("ksks22")
+//         // $(".is-selected").on('click', function () {
+//             console.log("ksks22")
+//         $(".is-selected").find(".moreInfoHover").removeClass("show");
+//         $(".carousel-cell").attr('tabindex', -1);
+//         // $(".carousel-cell").prop('disabled', true);
+//                     classTrue = false;
+//         // $("button").blur();
+        
+//     }
+
+//     else {
+//         // $(".carousel-cell").on('click', function () {
+//             // console.log("ksks")
+//         // $(".is-selected").attr("disabled", true);
+//         $(".is-selected").attr('tabindex', 0).find(".moreInfoHover").addClass("show");
+//         // $(".is-selected");
+//             // .find(".is-selected")
+//                 // });
+//         classTrue = true;
+//         console.log(classTrue)
+
+
+//     }
+//     })
+// }
+
+
+// //Start
+// app.init = function () {
+//     // $(".carousel-cell").attr('tabindex', -1);
+//     // app.cliksPut();
+    
+//     // $(".carousel-cell").attr('tabindex', -1);
+//     // $(".is-selected").attr('tabindex', 0);
+//     // document.getElementById("is-selected").focus();
+// };
+
+// //Document Ready
+// $(() => app.init());

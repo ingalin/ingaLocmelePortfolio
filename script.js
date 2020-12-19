cab341c92ba5853e64f5132a2993aa9bb7ff0155
ingalin <ingalin@inbox.lv>
2020-12-19 12:22:00 -0500

    section 3 improved

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const app = {};
 
-
-
 //Start
 app.init = function () {
-    // Stop Flickity carousel when focused within carousel
+// set Flickity autoplay
     const $carousel = $('.carousel').flickity({
         autoPlay: 2500
     });
-
+// Stop Flickity carousel when focused within carousel
     $(".carousel-cell").on('focusin', function () {
         $carousel.flickity('stopPlayer');
     });
